50a0c61febc0f8311f16b3961067ee5fe1cbc24d
kaurkaelepkhk <45162102+kaurkaelepkhk@users.noreply.github.com>
2018-12-18 14:00:49 +0200

    Numbri arvamise mäng

--- index.js
+++ index.js
@@ -24,29 +24,72 @@ const guessSubmit = document.querySelector('.guessSubmit');
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHigh = document.querySelector('.lowOrHigh');
+const reset = document.querySelector('.reset');
+const warning = document.querySelector('.warning');
 
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 
+// let arr = [];
+
 let guessCount = 1;
 
+let lowOrHighText = 'Viimane pakkumine oli liiga ';
+
 const checkGuess = () => {
+
     let userGuess = Number(guessField.value);
+
+    /* arr.push(userGuess);
+    console.log(arr); */
     if (guessCount === 1) {
         guesses.textContent = 'Eelnevad pakkumised: '
-    }
+    } /* else if (userGuess === arr.includes) {
+        warning.textContent = 'Juba kasutasid seda!';
+        guessCount--;
+    } */
     guesses.textContent += `${userGuess} `;
-
     if (userGuess === randomNumber) {
         //õigesti
-        lastResult.innerHTML="Arvasid õigesti!";
+        lastResult.textContent = 'Palju õnne, sa võitsid!';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHigh.textContent = '';
+        gameOver();
     } else if (guessCount === 10) {
         // lõpp
-    } else (userGuess < randomNumber) {
-        // liiga madal
-    } else if (userGuess > randomNumber) {
-        // liiga kõrge
+        lastResult.textContent = 'Palju õnne, sa kaotasid!';
+        lastResult.style.backgroundColor = 'red';
+        lowOrHigh.textContent = '';
+        gameOver();
+    } else {
+        lastResult.textContent = 'Vale vastus!';
+        lastResult.style.backgroundColor = 'red';
+        if (userGuess < randomNumber) {
+            // liiga madal
+            lowOrHigh.textContent = lowOrHighText + 'madal';
+        } else if (userGuess > randomNumber) {
+            // liiga kõrge
+            lowOrHigh.textContent = lowOrHighText + 'kõrge';
+        };
     };
     guessCount++;
+    guessField.value = '';
+    // mängu lõpp
+    function gameOver() {
+        guessField.disabled = true;
+        guessSubmit.style.display = 'none';
+        reset.style.display = 'inline-block';
+    }
+    // mängu reset
+    reset.addEventListener('click', function () {
+        guessCount = 1;
+        guessField.disabled = false;
+        guessSubmit.style.display = 'inline-block';
+        reset.style.display = 'none';
+        // nulli teksti sisu
+        lastResult.textContent = '';
+        lastResult.style.backgroundColor = '';
+        guesses.textContent = '';
+    })
 };
 
 guessSubmit.addEventListener('click', checkGuess);
